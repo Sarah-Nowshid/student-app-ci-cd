5d5c50c727e01092a912cb323c3d7d290ef8c7e5
Sarah Nowshid <sarahnowshid4@gmail.com>
2026-01-07 05:40:38 -0800

    Updated message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ students = []
 
 @app.route("/")
 def home():
-    return "Student Management System is Running yaaayyy"
+    return "Student Management System is Running"
 
 @app.route("/add-student", methods=["POST"])
 def add_student():
